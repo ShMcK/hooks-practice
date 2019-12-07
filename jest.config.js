7c375426bdec3be65f20086d70dddb980005b92d
shmck <shawn.j.mckay@gmail.com>
2019-12-07 13:49:12 -0800

    init

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	silent: false,
+
+	// Stop running tests after `n` failures
+	bail: 1,
+
+	// Use this configuration option to add custom reporters to Jest
+	// jest-tap-reporter is used by CodeRoad
+	reporters: ['jest-tap-reporter'],
+}
